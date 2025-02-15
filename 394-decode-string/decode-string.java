d5513df6b533b0862d3e35f71c29ba7206fd9cb4
shradda15 <129823516+shradda15@users.noreply.github.com>
2025-02-15 23:08:00 +0530

    Time: 1 ms (74.63%) | Memory: 41.6 MB (55.82%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -2,45 +2,31 @@ import java.util.Stack;
 
 class Solution {  
     public String decodeString(String s) {  
-        Stack<Character> sa = new Stack<>();  
-        Stack<Integer> si = new Stack<>();  
-        StringBuilder sb = new StringBuilder();  
-        int i = 0;  
+        Stack<Integer> countStack = new Stack<>();  
+        Stack<StringBuilder> stringStack = new Stack<>();  
+        StringBuilder currentString = new StringBuilder();  
+        int currentNumber = 0;  
 
-        while (i < s.length()) {  
-            char c = s.charAt(i);  
+        for (char c : s.toCharArray()) {  
             if (Character.isDigit(c)) {  
-                int num = 0;  
-                while (i < s.length() && Character.isDigit(s.charAt(i))) {  
-                    num = num * 10 + (s.charAt(i) - '0');  
-                    i++;  
-                }  
-                si.push(num);  
-                continue; // Skip incrementing i as it's already done in the loop  
-            } else if (c == '[' || (c >= 'a' && c <= 'z')) {  
-                sa.push(c);  
+                currentNumber = currentNumber * 10 + (c - '0');  
+            } else if (c == '[') {  
+                countStack.push(currentNumber);  
+                stringStack.push(currentString);  
+                currentString = new StringBuilder();  
+                currentNumber = 0;  
             } else if (c == ']') {  
-                StringBuilder temp = new StringBuilder();  
-                while (sa.peek() != '[') {  
-                    temp.insert(0, sa.pop()); // Use insert to maintain order  
+                StringBuilder decodedString = new StringBuilder(stringStack.pop());  
+                int repeatTimes = countStack.pop();  
+                for (int j = 0; j < repeatTimes; j++) {  
+                    decodedString.append(currentString);  
                 }  
-                sa.pop(); // pop the '['  
-                int repeat = si.pop();  
-                StringBuilder stb = new StringBuilder();  
-                for (int j = 0; j < repeat; j++) {  
-                    stb.append(temp);  
-                }  
-                for (char ch : stb.toString().toCharArray()) {  
-                    sa.push(ch); // Push each character back onto the stack  
-                }  
+                currentString = decodedString;   
+            } else {  
+                currentString.append(c);  
             }  
-            i++;  
-        }  
-        
-        while (!sa.isEmpty()) {  
-            sb.insert(0, sa.pop()); // Build final result from the stack  
         }  
 
-        return sb.toString();  
+        return currentString.toString();  
     }  
 }
