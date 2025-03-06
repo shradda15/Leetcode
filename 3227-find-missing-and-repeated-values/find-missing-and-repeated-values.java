8c9af338feaca5606dbb0afdb2724e954794f3a5
shradda15 <129823516+shradda15@users.noreply.github.com>
2025-03-06 20:54:14 +0530

    Time: 5 ms (44.38%) | Memory: 45.8 MB (26.65%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -2,24 +2,22 @@ class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n=grid.length;
         int[] a=new int[2];
-        List<Integer> l=new ArrayList<>();
+        Map<Integer,Integer> m=new HashMap<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(l.contains(grid[i][j])){
+                if(m.containsKey(grid[i][j])){
                     a[0]=grid[i][j];
                 }
                 else{
-                    l.add(grid[i][j]);
+                    m.put(grid[i][j],1);
                 }
             }
         }
-        Collections.sort(l);
-        System.out.print(l);
         for(int i=1;i<=n*n;i++){
-            if (!l.contains(i)) {  
-                a[1] = i;  
-                break;  
-            } 
+            if(!m.containsKey(i)){
+                a[1]=i;
+                break;
+            }
         }
         return a;
     }
