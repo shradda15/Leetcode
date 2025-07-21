14fad6723879594d4c128158de2608b201ca9c86
shradda15 <129823516+shradda15@users.noreply.github.com>
2025-07-21 12:52:41 +0530

    Time: 35 ms (57.83%) | Memory: 45.5 MB (92.42%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -0,0 +1,18 @@
+class Solution {  
+    public String makeFancyString(String s) {  
+        StringBuilder sb = new StringBuilder();  
+        int count = 1; 
+        sb.append(s.charAt(0)); 
+        for (int i = 1; i < s.length(); i++) {   
+            if (s.charAt(i) == s.charAt(i - 1)) {  
+                count++;  
+            } else {  
+                count = 1;   
+            }   
+            if (count <= 2) {  
+                sb.append(s.charAt(i));  
+            }  
+        }  
+        return sb.toString();  
+    }  
+}
