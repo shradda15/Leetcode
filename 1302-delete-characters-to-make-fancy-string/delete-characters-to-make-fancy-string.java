2d1669a9df4a7a94a3f36d9c56067fe5f49301bb
shradda15 <129823516+shradda15@users.noreply.github.com>
2025-07-21 13:16:19 +0530

    Time: 35 ms (57.83%) | Memory: 45.9 MB (40.66%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,18 +1,30 @@
-class Solution {  
-    public String makeFancyString(String s) {  
-        StringBuilder sb = new StringBuilder();  
-        int count = 1; 
-        sb.append(s.charAt(0)); 
-        for (int i = 1; i < s.length(); i++) {   
-            if (s.charAt(i) == s.charAt(i - 1)) {  
-                count++;  
-            } else {  
-                count = 1;   
-            }   
-            if (count <= 2) {  
-                sb.append(s.charAt(i));  
-            }  
-        }  
-        return sb.toString();  
-    }  
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder sb=new StringBuilder();
+        int i=0, j=1,c=0;
+        while(j<s.length()){
+            if(s.charAt(i)!=s.charAt(j)){
+                sb.append(s.charAt(i));
+                if(c>=1){
+                    sb.append(s.charAt(i));
+                }
+                i=j;
+                // System.out.println(i);
+                j++;
+                c=0;
+            }
+            else{
+                j++;
+                c++;
+            }
+        }
+        if(s.length()-1-i>=1){
+            sb.append(s.charAt(i));
+            sb.append(s.charAt(i));
+        }
+        else{
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
 }
